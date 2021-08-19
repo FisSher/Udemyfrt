7e2b5e4ec1e85b43debba89bccfbc5ea2b0b42f8
Facundo <facundo.paolini@gmail.com>
2021-08-19 13:45:40 -0300

    Added new endpoint to test

--- src/test/java/Steps/APISteps.java
+++ src/test/java/Steps/APISteps.java
@@ -6,8 +6,14 @@ import io.restassured.response.Response;
 import io.restassured.response.Validatable;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
 
 
 public class APISteps {
@@ -16,21 +22,34 @@ public class APISteps {
     private Response response;
     private ValidatableResponse json;
 
-    @Given("^a GET request is sent to the endpoint$")
-    public void sendGETRequest(){
+    @Given("^a GET request is sent to the (.+) endpoint$")
+    public void sendGETRequest(String URI) {
         request = given()
-                .baseUri("https://api.github.com")
+                .baseUri(URI)
                 .contentType(ContentType.JSON);
-
-
     }
-
 
     @Then("^a (\\d+) status code is received$")
     public void validateListOfUsers(int expecteStatusCode) {
         response = request
-                    .when()
-                    .get("users/FisSher/repos");
+                .when()
+                .get("users/FisSher/repos");
         json = response.then().statusCode(expecteStatusCode);
+    }
+
+    @Then("^(\\d+) items are present in the (.+) endpoint$")
+    public void itemsArePresentInTheEndpoint(int expectedSize, String endpoint) {
+        response = request
+                .when()
+                .get(endpoint);
+        Assertions.assertEquals(response.getStatusCode(),200);
+
+        List<String> jsonResponse = response.jsonPath().getList("$");
+        int actualSize = jsonResponse.size();
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(expectedSize, actualSize),
+                () -> Assertions.assertEquals("b", "a"),
+                () -> Assertions.assertEquals("a", "a")
+        );
     }
 }
--- src/test/resources/features/APITest.feature
+++ src/test/resources/features/APITest.feature
@@ -1,5 +1,11 @@
 Feature: Request example
+
     @API
     Scenario: GET from endpoint test
-    Given a GET request is sent to the endpoint
-    Then a 200 status code is received
+        Given a GET request is sent to the https://api.github.com endpoint
+        Then a 200 status code is received
+
+     @API
+     Scenario: Validate item quantity in response
+        Given a GET request is sent to the https://jsonplaceholder.typicode.com/ endpoint
+        Then 10 items are present in the /users endpoint
